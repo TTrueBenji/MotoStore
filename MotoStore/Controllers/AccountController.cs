b3f22ca68b8b57b7d2ac26d04571f01c2a62670f
Виталий Пасько <pasko.vitaliy24@gmail.com>
2022-03-10 00:47:45 +0600

    Добавил контроллер и страницу регистрации.
    #1

---
+++ MotoStore/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MotoStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
